8139ce17755f9923e1c25979e8e10fa14aee4db5
John McCance <mccance@usc.edu>
2024-04-15 21:54:01 +0000

    Hello

--- schedwork.cpp
+++ schedwork.cpp
@@ -23,7 +23,12 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 // Add prototypes for any helper functions here
 
 
-// Add your implementation of schedule() and other helper functions here
+
+
+bool helper(const AvailabilityMatrix& avail, int dailyNeed, DailySchedule& sched, int day, int worker, DailySchedule& temp, vector<int>& remainingDays, const int originalDailyNeed);
+
+
+//Add your implementation of schedule() and other helper functions here
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -38,8 +43,62 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+		
+		//create whats in the matrix??
+	  vector<int> remainingDays(avail[0].size(), maxShifts); 
+		DailySchedule temp(avail.size()); 
+	
+		const int originalDailyNeed = dailyNeed; 
 
+
+		
+
+		return helper(avail, dailyNeed, sched, 0, 0, temp, remainingDays, originalDailyNeed); 
+
+
+	
+}
+
+
+bool helper(const AvailabilityMatrix& avail, int dailyNeed, DailySchedule& sched, int day, int worker, DailySchedule& temp, vector<int>& remainingDays, const int originalDailyNeed){
+
+
+
+	if(day >= avail.size()){
+
+		sched = temp; 
+
+
+		return true; 
+
+	}
+	if (dailyNeed == 0) {
+		return helper(avail, originalDailyNeed, sched, day + 1, 0, temp, remainingDays, originalDailyNeed); 
+	}
+	if(worker >= avail[0].size()){
+		return false;
+	}
+	//once you go past the last worker, you will check if you have used up daily need workers 
+  
+	bool answer = false; 
+
+	if(avail[day][worker] == true && remainingDays[worker] > 0){
+		temp[day].push_back(worker); 
+		remainingDays[worker]--; 
+	
+		answer = answer || helper(avail, dailyNeed - 1, sched, day, worker + 1, temp, remainingDays, originalDailyNeed); 
+	  temp[day].pop_back(); 
+		remainingDays[worker]++; 
+
+	}
+
+	answer = answer || helper(avail, dailyNeed, sched, day, worker + 1, temp, remainingDays, originalDailyNeed); 
+
+	return answer; 
 
 
 }
+
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -15,6 +15,10 @@ using namespace std;
 // Add prototypes of helper functions here
 
 
+void backtracking(std::string& in, const std::set<std::string>& dict, std::string floating, int x, std::set<std::string>& holder); 
+
+
+
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -23,6 +27,69 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+	//one string in which is the string you want to add letters to, have floating letters and dictionary that has all the words
+	//iterate through all charachters and your out of floating letters and you have a valid word 
+
+	
+	std::set<std::string> holder; 
+	std::string floating2 = floating; 
+
+
+  std::string in2 = in; 
+
+
+	backtracking(in2, dict, floating2, 0, holder); 
+	return holder; 
+
+
+	
+
 }
 
 // Define any helper functions here
+
+void backtracking(std::string& in, const std::set<std::string>& dict, std::string floating, int x, std::set<std::string>& holder){
+
+	//in is the 
+	//if something is already in IN then dont overwrite it, if its a - then can change
+	//increment x whenever you change a dash 
+	if(x == in.length()){
+		if(dict.find(in) != dict.end() && floating.size() == 0){
+			holder.insert(in); 
+		}
+		return;
+	}
+
+	//need to check if floating charachter 
+	if(in[x] == '-'){
+
+		int dashCount = 1;
+		for (int curr = x+1; curr < in.length(); curr++) {
+			if (in[curr] == '-') dashCount++;
+		}
+		
+		if (dashCount > floating.length()) {		
+			for(int i = 'a'; i <= 'z'; i++){
+				in[x] = i; 
+				backtracking(in, dict, floating, x + 1, holder); 
+			}
+		}
+
+		string prev = floating; 
+		for(int j = 0; j < prev.size(); j++){
+			floating = prev.substr(0, j) + prev.substr(j + 1);
+			in[x] = prev[j];
+			backtracking(in, dict, floating, x + 1, holder);
+		}
+
+		in[x] = '-';
+	}
+	else{
+		backtracking(in, dict, floating, x + 1, holder); 
+
+	}
+
+}
+
+
+
